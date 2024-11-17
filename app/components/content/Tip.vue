e67f7f626de6652c36330487a9689ad9f72accac
skkylimits <skkylimits@gmail.com>
2024-11-17 02:42:28 +0100

    fix: border to text-red-700

--- app/components/content/Tip.vue
+++ app/components/content/Tip.vue
@@ -2,8 +2,8 @@
     <Alert 
         icon="icon-park-solid:caution" 
         title="Tip"
-        iconColor="text-green-400 dark:bg-border-green-900"
-        class="bg-[#d2f8d2] dark:bg-green-700 text-black dark:text-white border-2 border-green-400 dark:bg-border-green-900"
+        iconColor="text-green-400 dark:bg-text-green-900"
+        class="bg-[#d2f8d2] dark:bg-green-700 text-black dark:text-white border-2 border-green-400 dark:bg-border-green-200"
     >
         <slot />
     </Alert>
