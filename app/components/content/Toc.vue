ca9ac64dfe8148e1e73136fe17db1d1f0bc0cf6a
skkylimits <skkylimits@gmail.com>
2024-11-30 23:02:19 +0100

    fix: update tabel in netspeed and added TOC mdc component

---
+++ app/components/content/Toc.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const { data: page } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+</script>
+
+<template>
+  <UContentToc :links="page.body.toc.links" />
+</template>
