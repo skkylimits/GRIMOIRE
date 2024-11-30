0a27286f9adc00068522cc9c7d8d947e1f065f0e
skkylimits <skkylimits@gmail.com>
2024-12-01 00:19:19 +0100

    freestyle way of using faq MDC instead of hardcoded in script

--- app/components/content/Faq.vue
+++ app/components/content/Faq.vue
@@ -8,14 +8,6 @@ const items = [
     label: 'Can I use Nuxt UI Pro for Open Source projects?',
     content: 'Et adipisicing do do do sunt irure proident consequat fugiat tempor occaecat commodo fugiat in proident.'
   },
-  {
-    label: 'What does “Unlimited minor & patch updates” include?',
-    content: 'Dolor dolor consectetur tempor consectetur sint ut id ex quis voluptate dolore incididunt qui mollit.'
-  },
-  {
-    label: 'Do you offer technical support?',
-    content: 'Sint id sint incididunt culpa.'
-  }
 ]
 </script>
 
@@ -23,15 +15,10 @@ const items = [
   <ULandingFAQ :items="items">
   </ULandingFAQ>
 </template>
-
+ 
 
 <style>
 button[data-headlessui-state="open"] span {
   color: rgb(var(--color-primary-DEFAULT) / var(--tw-text-opacity)) !important;
-}
-
-/* Adding smooth transition for better UX */
-::v-deep button span {
-  transition: color 0.3s ease;
 }
 </style>
---
+++ app/components/content/Test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <details v-for="(item, index) in items" :key="index">
+        <summary>{{ item.question }}</summary>
+        <p>{{ item.answer }}</p>
+      </details>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    items: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  });
+  </script>
+  
