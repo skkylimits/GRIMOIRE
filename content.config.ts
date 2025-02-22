e404ee36a7dfa267e653e0fa507b65cdec8ec1ca
skkylimits <skkylimits@gmail.com>
2025-02-22 18:30:52 +0100

    _dir.yml to .navigation.yml

--- content.config.ts
+++ content.config.ts
@@ -10,7 +10,7 @@ export default defineContentConfig({
 			type: 'page',
 			source: {
 				include: '**',
-				exclude: ['index.md']
+				exclude: ['**/index.md', '**/.!(navigation.yml)']
 			},
 			schema: z.object({
 				links: z.array(z.object({
