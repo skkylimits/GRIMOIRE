e7d3c65a0968c709d9157a808acfcd0b152bf544
skkylimits <skkylimits@gmail.com>
2025-10-11 02:04:04 +0200

    weve made it

--- app/pages/debug-auth.vue
+++ app/pages/debug-auth.vue
@@ -1,6 +1,4 @@
-<!-- eslint-disable -->
-<!-- @ts-nocheck -->
-
+<!-- eslint-disable no-console -->
 <script setup lang="ts">
 const { status, data: session } = useAuth()
 
@@ -17,27 +15,5 @@ onMounted(() => {
 </script>
 
 <template>
-	<div class="p-6 space-y-4">
-		<h1 class="text-2xl font-bold">
-			Auth Debug
-		</h1>
-
-		<p>Status: <strong>{{ status }}</strong></p>
-
-		<pre class="bg-gray-900 text-white p-3 rounded">
-      {{ JSON.stringify(session, null, 2) }}
-    </pre>
-
-		<UButton
-			v-if="status === 'authenticated'"
-			label="Sign out"
-			@click="signOut()"
-		/>
-		<UButton
-			v-else
-			label="Sign in with GitHub"
-			icon="i-simple-icons-github"
-			@click="signIn('github')"
-		/>
-	</div>
+	<AuthStatusCard />
 </template>
